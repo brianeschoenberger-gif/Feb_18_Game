78259627ed353cc05469b496bed46c26deae9128
Brian Schoenberger <12345678+brianeschoenberger-gif@users.noreply.github.com>
2026-02-18 11:54:53 -0800

    Set Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Feb_18_Game/",
   define: {
     "typeof CANVAS_RENDERER": JSON.stringify(true),
     "typeof WEBGL_RENDERER": JSON.stringify(true)
